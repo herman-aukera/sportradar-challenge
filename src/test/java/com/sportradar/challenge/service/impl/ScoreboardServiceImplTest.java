51cdddf24489cba1558a53ad1b5bcd4c3f73ffe9
Gonzalo (Herman) Gonzalez <herman.gonzalez@pm.me>
2024-05-07 11:02:37 +0200

    Expand test cases for Summary

--- src/test/java/com/sportradar/challenge/service/impl/ScoreboardServiceImplTest.java
+++ src/test/java/com/sportradar/challenge/service/impl/ScoreboardServiceImplTest.java
@@ -85,16 +85,41 @@ public class ScoreboardServiceImplTest {
         scoreboard.startNewMatch("Spain", "Brazil");
         scoreboard.updateScore("Spain", "Brazil", 10, 2);
 
+        scoreboard.startNewMatch("Germany", "France");
+        scoreboard.updateScore("Germany", "France", 2, 2);
+
+        scoreboard.startNewMatch("Uruguay", "Italy");
+        scoreboard.updateScore("Uruguay", "Italy", 6, 6);
+
+        scoreboard.startNewMatch("Argentina", "Australia");
+        scoreboard.updateScore("Argentina", "Australia", 3, 1);
+
         List<Match> summary = scoreboard.getSummary();
 
-        assertEquals("Spain", summary.get(0).homeTeam());
-        assertEquals("Brazil", summary.get(0).awayTeam());
-        assertEquals(10, summary.get(0).homeScore());
-        assertEquals(2, summary.get(0).awayScore());
+        // Matches with the same total score should be ordered by the most recently started match
+        assertEquals("Uruguay", summary.get(0).homeTeam());
+        assertEquals("Italy", summary.get(0).awayTeam());
+        assertEquals(6, summary.get(0).homeScore());
+        assertEquals(6, summary.get(0).awayScore());
 
-        assertEquals("Mexico", summary.get(1).homeTeam());
-        assertEquals("Canada", summary.get(1).awayTeam());
-        assertEquals(0, summary.get(1).homeScore());
-        assertEquals(5, summary.get(1).awayScore());
+        assertEquals("Spain", summary.get(1).homeTeam());
+        assertEquals("Brazil", summary.get(1).awayTeam());
+        assertEquals(10, summary.get(1).homeScore());
+        assertEquals(2, summary.get(1).awayScore());
+
+        assertEquals("Mexico", summary.get(2).homeTeam());
+        assertEquals("Canada", summary.get(2).awayTeam());
+        assertEquals(0, summary.get(2).homeScore());
+        assertEquals(5, summary.get(2).awayScore());
+
+        assertEquals("Argentina", summary.get(3).homeTeam());
+        assertEquals("Australia", summary.get(3).awayTeam());
+        assertEquals(3, summary.get(3).homeScore());
+        assertEquals(1, summary.get(3).awayScore());
+
+        assertEquals("Germany", summary.get(4).homeTeam());
+        assertEquals("France", summary.get(4).awayTeam());
+        assertEquals(2, summary.get(4).homeScore());
+        assertEquals(2, summary.get(4).awayScore());
     }
 }
